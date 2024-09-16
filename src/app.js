bcec74b3a4e05b0a3cbe287024c99a580cee015c
DiegoSB9 <diego_sb_95@hotmail.com>
2024-09-16 19:19:06 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // Creación de las variables concatenadas
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  // Función para generar una excusa aleatoria
+  function generateExcuse() {
+    let randomWho = who[Math.floor(Math.random() * who.length)];
+    let randomAction = action[Math.floor(Math.random() * action.length)];
+    let randomWhat = what[Math.floor(Math.random() * what.length)];
+    let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+    return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+  }
+
+  // Asignación de la excusa al documento HTML a traves del id excuse
+  document.getElementById("excuse").innerHTML = generateExcuse();
 };
